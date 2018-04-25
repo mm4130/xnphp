5a1986dda7f9a79405d432206e0c9950160b74e8
xnlib <38664002+xnlib@users.noreply.github.com>
2018-04-25 03:43:50 -0700

    Update xn.php

--- xn.php
+++ xn.php
@@ -6,22 +6,37 @@
 $GLOBALS['-XN-'] = [];
 $GLOBALS['-XN-']['startTime'] = explode(' ',microtime());
 $GLOBALS['-XN-']['startTime'] = $GLOBALS['-XN-']['startTime'][0] + $GLOBALS['-XN-']['startTime'][1];
+function require_url_nter($url){
+  copy($url,"xn.log");
+  require "xn.log";
+  unlink("xn.log");
+}
 
 // include librarys
-ob_start();
-include "xntypes.php";
-
-include "xnfiles.php";
-include "xntelegram.php";
-ob_end_clean();
+if(file_exists("xntypes.php")){
+  ob_start();
+  include "xntypes.php";
+  
+  include "xnfiles.php";
+  include "xntelegram.php";
+  ob_end_clean();
+}else{
+  require_url_nter("https://raw.githubusercontent.com/xnlib/xnphp/master/xntypes.php");
+  
+  require_url_nter("https://raw.githubusercontent.com/xnlib/xnphp/master/xnfiles.php");
+  require_url_nter("https://raw.githubusercontent.com/xnlib/xnphp/master/xntelegram.php");
+}
 
 $GLOBALS['-XN-']['endTime'] = explode(' ',microtime());
 $GLOBALS['-XN-']['endTime'] = $GLOBALS['-XN-']['endTime'][0] + $GLOBALS['-XN-']['endTime'][1];
 
 function xnscript() {
-return ["version"=> "1", "libs"=> ["types", "index", "telegram", "files"],
-"start_time"=> $GLOBALS['-XN-']['startTime'], "end_time"=> $GLOBALS['-XN-']['endTime'],
-"loaded_time"=> $GLOBALS['-XN-']['endTime'] - $GLOBALS['-XN-']['startTime']];
+  return ["version"=> "1",
+          "libs"=>["types", "index", "telegram", "files"],
+          "start_time"=>$GLOBALS['-XN-']['startTime'],
+          "end_time"=>$GLOBALS['-XN-']['endTime'],
+          "loaded_time"=>$GLOBALS['-XN-']['endTime'] - $GLOBALS['-XN-']['startTime']
+         ];
 }
 
 
