35d54e76f48af314cb48d500bc7be7ff218689cb
xnlib <38664002+xnlib@users.noreply.github.com>
2018-04-25 13:26:35 -0700

    Add files via upload

---
+++ xntime.php
@@ -0,0 +1,18 @@
+<?php
+
+// Created by whiteweb // xn.white-web.ir // @white_web
+// xn plugin time v1
+
+function xndateoption($date=1){
+if($date==2)return -19603819800;
+if($date==3)return -18262450800;
+return 0;
+}function xntimeoption($time){
+return (new DateTime(null,new DateTimeZone($time)))->getOffset();
+}function xntime($option=0){
+return microtime(true)+$option;
+}function xndate($date="c",$option=0){
+return date($date,xntime($option));
+}
+
+?>
