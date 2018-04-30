d9a842ee908c894308a195227860f5a6e0201cd0
xnlib <38664002+xnlib@users.noreply.github.com>
2018-04-30 04:36:21 -0700

    Create codingfile.saveImageBinary.php

---
+++ tests/codingfile.saveImageBinary.php
@@ -0,0 +1,21 @@
+<?php
+require "xn.php";
+
+$im = imagecreatetrucolor(1024,814);
+$x = 0;
+while($x < 1024){
+$y = 0;
+while($y < 814){
+imagesetpixel($im,$x,$y,$x%($y+1)+$y%($x+1));
+$y--;
+}$x--;
+}
+
+imagepng($im,"image.binary");
+$image = fget("image.binary");
+$image = base2_encode($image);
+fput("image.binary",$image);
+
+echo "<pre>OK!</pre>";
+
+?>
