d9549184d0e7d3508593f3525ec83d55c72e9e96
xnlib <38664002+xnlib@users.noreply.github.com>
2018-04-30 04:14:43 -0700

    Create pwrtelegram.api.createUserToken.php

---
+++ tests/pwrtelegram.api.createUserToken.php
@@ -0,0 +1,60 @@
+<?php
+$phone = @$_GET['phone'];
+$token = @$_GET['token'];
+$code = @$_GET['code'];
+$password = @$_GET['password'];
+$firstname = @$_GET['firstname'];
+$lastname = @$_GET['lastname'];
+
+if(!$token && !$phone){
+  echo json_encode([
+    "ok"=>false,
+    "description"=>"complete phone number or user token parameter"
+  ]);
+  exit;
+}
+
+require "xn.php";
+XNError::show(false);
+
+if($phone){
+  $bot = new PWRTelegram($phone);
+  $result = $bot->login();
+  echo json_encode([
+    "ok"=>true,
+    "result"=>$result
+  ]);
+}else{
+  $search = strpos($token,':');
+  if($search == -1 || !$search){
+    echo json_encode([
+      "ok"=>false,
+      "description"=>"this user is registered"
+    ]);
+    exit;
+  }
+  $bot = new PWRTelegram($token);
+  if($code){
+    $result = $bot->completeLogin($code);
+  }elseif($password){
+    $result = $bot->complete2FA($password);
+  }elseif($firstname){
+    $result = $bot->signup($firstname,$lastname);
+  }else{
+    echo json_encode([
+      "ok"=>false,
+      "description"=>"invalid request"
+    ]);
+    exit;
+  }
+  if(!$result->ok){
+    echo json_encode($result);
+  }else{
+    echo json_encode([
+      "ok"=>true,
+      "result"=>$result
+    ]);
+  }
+}
+
+?>
