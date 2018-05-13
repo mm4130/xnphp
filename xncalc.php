fc824fd460e1ce3021dd733611fc52076b994d2f
xnlib <38664002+xnlib@users.noreply.github.com>
2018-05-13 09:27:19 -0700

    Add files via upload

--- xncalc.php
+++ xncalc.php
@@ -90,6 +90,8 @@ $p=self::_lm($a);
 return $p!==false&&$p!=-1;
 }static function _nm($a){
 return str_replace('.','',$a);
+}static function _st($a,$b){
+return substr_replace($a,'.',$b,0);
 }static function _iz($a){
 $a=$a[strlen($a)-1];
 return $a=='0'||$a=='2'||$a=='4'||$a=='6'||$a=='8';
@@ -100,6 +102,12 @@ return $a=='1'||$a=='3'||$a=='5'||$a=='7'||$a=='9';
 $l=strlen($a)%$b;
 if($l==0)return $a;
 else return str_repeat('0',$b-$l).$a;
+}static function _pl($a){
+$l='0';
+while($a!=$l){
+$l=$a;
+$a=str_replace(['--','-+','+-','++'],['+','-','-','+'],$a);
+}return $a;
 }
 // retry calc functions
 static function _powTen0($a,$b){
@@ -121,6 +129,38 @@ return self::_powTen0(self::_not($a),$b);
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 return self::_get(self::_powTen1($a,$b));
+}static function _powTow0($a){
+$a=subsplit($a,13);
+$c=count($a)-1;
+while($c>=0){
+$a[$c]*=2;
+$k=0;
+while(@$a[$c-$k]>9999999999999){
+$a[$c-$k-1]+=1;
+$a[$c-$k]-=10000000000000;
+$k++;
+}$a[$c]=self::_so($a[$c],13);
+$c--;
+}return implode('',$a);
+}static function _powTow1($a){
+$a=self::_mo($a);
+$a[0]=self::_so($a[0],13);
+$a[0]=self::_powTow0("0000000000000{$a[0]}");
+if(isset($a[1])){
+$l=strlen($a[1]);
+$a[1]=self::_so($a[1],13);
+$a[1]=self::_powTow0("0000000000000{$a[1]}");
+$a[2]=substr($a[1],0,-$l);
+$a[1]=substr($a[1],-$l);
+if($a[2]>0)$a[0]=self::_add0("0000000000000{$a[0]}","0000000000000".str_repeat('0',strlen($a[0])-1).'1');
+return "{$a[0]}.{$a[1]}";
+}return $a[0];
+}static function _powTow2($a){
+if(self::_view($a))return '-'.self::_powTow1(self::_not($a));
+return self::_powTow1(self::_not($a));
+}static function powTow($a){
+if(!self::_check($a))return false;
+return self::_get3(self::_powTow2(self::_get3($a)));
 }
 // calc functions
 static function _add0($a,$b){
@@ -138,21 +178,11 @@ $k++;
 $c--;
 }return implode('',$a);
 }static function _add1($a,$b){
-$a=self::_mo($a);
-$b=self::_mo($b);
-$a[0]=self::_so($a[0],13);
-$b[0]=self::_so($b[0],13);
-$a[0]=self::_add0("0000000000000{$a[0]}","0000000000000{$b[0]}");
-if(isset($a[1])){
-$l=strlen($a[1]);
-$a[1]=self::_so($a[1],13);
-$b[1]=self::_so($b[1],13);
-$a[1]=self::_add0("0000000000000{$a[1]}","0000000000000{$b[1]}");
-$a[2]=substr($a[1],0,-$l);
-$a[1]=substr($a[1],-$l);
-if($a[2]>0)$a[0]=self::_add0("0000000000000{$a[0]}","0000000000000".str_repeat('0',strlen($a[0])-1).'1');
-return "{$a[0]}.{$a[1]}";
-}return $a[0];
+$p=self::_lm($a)+(13-(strlen($a)-1)%13);
+$a=self::_so(self::_nm($a),13);
+$b=self::_so(self::_nm($b),13);
+if($p!==false&&$p!==-1)return self::_st(self::_add0($a,$b),$p);
+return self::_add0($a,$b);
 }static function _add2($a,$b){
 if( self::_view($a)&& self::_view($b))return '-'.self::_add1(self::_not($a),self::_not($b));
 if( self::_view($a)&&!self::_view($b))return     self::_rem1(self::_not($b),self::_not($a));
@@ -162,7 +192,41 @@ if(!self::_view($a)&& self::_view($b))return     self::_rem1(self::_not($a),self
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 self::_setfull($a,$b);
-return self::_get(self::_add2($a,$b));
+return self::_get3(self::_add2($a,$b));
+}public function _rem0($a,$b){
+$a=subsplit($a,13);
+$b=subsplit($b,13);
+$c=count($a)-1;
+while($c>=0){
+$a[$c]-=$b[$c];
+$k=0;
+while(@$a[$c-$k]<0){
+$a[$c-$k-1]-=1;
+$a[$c-$k]+=10000000000000;
+$k++;
+}$a[$c]=self::_so($a[$c],13);
+$c--;
+}return implode('',$a);
+}static function _rem1($a,$b){
+$p=self::_lm($a)+(13-(strlen($a)-1)%13);
+$a=self::_so(self::_nm($a),13);
+$b=self::_so(self::_nm($b),13);
+if($p!==false&&$p!==-1)return self::_st(self::_rem0($a,$b),$p);
+return self::_rem0($a,$b);
+}static function _rem2($a,$b){
+if( self::_view($a)&& self::_view($b))return '-'.self::_rem1(self::_not($a),self::_not($b));
+if( self::_view($a)&&!self::_view($b))return '-'.self::_add1(self::_not($a),self::_not($b));
+if(!self::_view($a)&& self::_view($b))return     self::_add1(self::_not($a),self::_not($b));
+                                      return     self::_rem1(self::_not($a),self::_not($b));
+}static function _rem3($a,$b){
+if($a<$b){
+return '-'.self::_rem2($b,$a);
+}return self::_rem2($a,$b);
+}static function rem($a,$b){
+if(!self::_check($a))return false;
+if(!self::_check($b))return false;
+self::_setfull($a,$b);
+return self::_pl(self::_get3(self::_rem3($a,$b)));
 }
 // run functions
 static function fromNumberString($a='0'){
