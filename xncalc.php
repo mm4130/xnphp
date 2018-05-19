c82861129bc57f24099c650b67ddbb3cbfa9324f
xnlib <38664002+xnlib@users.noreply.github.com>
2018-05-19 02:43:33 -0700

    Add files via upload

--- xncalc.php
+++ xncalc.php
@@ -131,7 +131,7 @@ return self::_powTen0(self::abs($a),$b);
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 return self::_get(self::_powTen1($a,$b));
-}static function _powTwo0($a){
+}static function _mulTwo0($a){
 $a=subsplit($a,13);
 $c=count($a)-1;
 while($c>=0){
@@ -144,25 +144,25 @@ $k++;
 }$a[$c]=self::_so($a[$c],13);
 $c--;
 }return implode('',$a);
-}static function _powTwo1($a){
+}static function _mulTwo1($a){
 $a=self::_mo($a);
 $a[0]=self::_so($a[0],13);
-$a[0]=self::_powTwo0("0000000000000{$a[0]}");
+$a[0]=self::_mulTwo0("0000000000000{$a[0]}");
 if(isset($a[1])){
 $l=strlen($a[1]);
 $a[1]=self::_so($a[1],13);
-$a[1]=self::_powTwo0("0000000000000{$a[1]}");
+$a[1]=self::_mulTwo0("0000000000000{$a[1]}");
 $a[2]=substr($a[1],0,-$l);
 $a[1]=substr($a[1],-$l);
 if($a[2]>0)$a[0]=self::_add0("0000000000000{$a[0]}","0000000000000".str_repeat('0',strlen($a[0])-1).'1');
 return "{$a[0]}.{$a[1]}";
 }return $a[0];
-}static function _powTwo2($a){
-if(self::_view($a))return '-'.self::_powTwo1(self::abs($a));
-return self::_powTwo1(self::abs($a));
-}static function powTwo($a){
+}static function _mulTwo2($a){
+if(self::_view($a))return '-'.self::_mulTwo1(self::abs($a));
+return self::_mulTwo1(self::abs($a));
+}static function mulTwo($a){
 if(!self::_check($a))return false;
-return self::_get3(self::_powTwo2(self::_get3($a)));
+return self::_get3(self::_mulTwo2(self::_get3($a)));
 }static function _divTwo0($a){
 $s='';
 $c=0;
@@ -190,6 +190,39 @@ if(self::_view($a))return '-'.self::_divTwo1(self::abs($a));
 return self::_divTwo1(self::abs($a));
 }static function divTwo($a){
 return self::_get(self::_divTwo2(self::_get($a)));
+}static function _powTwo0($a){
+$a=subsplit($a,1);
+$x=false;
+$c=$d=count($a)-1;
+$k=0;
+while($c>=0){
+$y='';
+$e=$d;
+$s=0;
+while($e>=0){
+$t=$a[$c]*$a[$e]+$s;
+$s=floor($t/10);
+$t-=$s*10;
+$y=$t.$y;
+$e--;
+}$c--;
+$t=$s.$y.($k?str_repeat('0',$k):'');
+$x=$x?self::add($x,$t):$t;
+$k++;
+}return $x;
+}static function _powTwo1($a){
+$p=self::_lm($a);
+if(!$p)return self::_powTwo0($a);
+$p=strlen($a)-$p-1;
+$p*=2;
+$a=self::_nm($a);
+$a='0'.self::_powTwo0($a);
+return self::_st($a,strlen($a)-$p);
+}static function _powTwo2($a){
+return self::_powTwo1(self::abs($a));
+}static function powTwo($a){
+if(!self::_check($a))return false;
+return self::_get3(self::_powTwo2(self::_get3($a)));
 }
 // set functions
 static function floor($a){
@@ -235,7 +268,10 @@ if(!self::_view($a)&& self::_view($b))return     self::_rem1(self::abs($a),self:
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 self::_setfull($a,$b);
-return self::_get3(self::_add2($a,$b));
+$r=$a==0?$b:
+   $b==0?$a:
+   self::_add2($a,$b);
+return self::_get3($r);
 }public function _rem0($a,$b){
 $a=subsplit($a,13);
 $b=subsplit($b,13);
@@ -270,7 +306,10 @@ return '-'.self::_rem2($b,$a);
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 self::_setfull($a,$b);
-return self::_pl(self::_get3(self::_rem3($a,$b)));
+$r=$a==0?self::_change($b):
+   $b==0?$a:
+   self::_rem3($a,$b);
+return self::_pl(self::_get3($r));
 }static function _mul0($a,$b){
 $a=subsplit($a,1);
 $b=subsplit($b,1);
@@ -312,12 +351,19 @@ if( self::_view($a)&&!self::_view($b))return '-'.self::_mul1(self::abs($a),self:
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 self::_setfull($a,$b);
-return self::_get3(self::_mul2($a,$b));
+$r=$b==0?0:
+   $b==1?$a:
+   $b==2?self::mulTwo($a):
+   $a==0?0:
+   $a==$b?powTwo($a):
+   $a==1?$b:
+   self::_mul2($a,$b);
+return self::_get3($r);
 }static function _rand0($a){
 $rand="0.";
 $b=floor($a/9);
 for($c=0;$c<$b;$c++){
-$rand=self::_so(rand(0,999999999),9);
+$rand.=self::_so(rand(0,999999999),9);
 }if($a%9==0)return $rand;
 return $rand.self::_so(rand(0,str_repeat('9',$a%9)),$a%9);
 }static function _rand1($a,$b){
@@ -346,7 +392,34 @@ return self::_change(self::rem(self::_rand3('0',self::add(self::abs($a),self::ab
 if(!self::_check($a))return false;
 if(!self::_check($b))return false;
 self::_setfull($a,$b);
-return self::_get(self::_rand4($a,$b));
+$r=$a==$b?$a:
+   self::_rand4($a,$b);
+return self::_get($r);
+}static function _div0($a,$b){
+if($b>$a)return 0;
+if(($c=self::mulTwo($b))>$a)return 1;
+if(self::mul($b,'3')>$a)return 2;
+if(($c=self::mulTwo($c))>$a)return 3;
+if(self::mul($b,'5')>$a)return 4;
+if(self::mul($b,'6')>$a)return 5;
+if(self::mul($b,'7')>$a)return 6;
+if(self::mulTwo($c)>$a)return 7;
+if(self::mul($b,'9')>$a)return 8;
+                        return 9;
+}static function _div2($a,$b,$c=0){
+$a=subsplit($a,1);
+$p=$r=$i=$d='0';
+$c=count($a);
+while($i<$c){
+$d.=$a[$i];
+if($d>=$b){
+$p=self::_div0($d,$b);
+$d=self::rem($d,self::_mul($p,$b));
+$r.=$p;
+}else $d.='0';
+$i++;
+}if($c==0||$d==0)return $r;
+return $d;
 }
 // run functions
 static function fromNumberString($a='0'){
