b400b4942e0643eb4142bf7a8143ca0edc91deee
xnlib <38664002+xnlib@users.noreply.github.com>
2018-05-20 12:21:35 -0700

    add new type for dateoption

--- xntime.php
+++ xntime.php
@@ -6,6 +6,7 @@
 function xndateoption($date=1){
 if($date==2)return -19603819800;
 if($date==3)return -18262450800;
+if($date==4)return -62167219200;
 return 0;
 }function xntimeoption($time){
 return (new DateTime(null,new DateTimeZone($time)))->getOffset();
