bedbcc0adfb74fb83bd0c13e93659873c6b5bb6a
xnlib <38664002+xnlib@users.noreply.github.com>
2018-05-22 04:45:38 -0700

    change model to function

--- xndata.php
+++ xndata.php
@@ -3,10 +3,8 @@
 // Created by avid
 // xn plugin data v1
 
-$GLOBALS['-XN-']['xnData']="xndata.txt";
-class XNData {
-static function get($data){
-$f=$GLOBALS['-XN-']['xnData'];
+function get_xndata($data){
+$f=$GLOBALS['-XN-']['dirName'].DIRECTORY_SEPARATOR.'xndata.txt';
 $leng=strlen($data);
 if(file_exists($f)){
 $f=fopen($f,'r');
@@ -22,12 +20,6 @@ $data=fread($f,$size-1);
 break;
 }fseek($f,$size,1);
 }return unserialize(zlib_decode($data));
-}static function download(){
-$file=$GLOBALS['-XN-']['xnData'];
-return copy("https://github.com/xnlib/xnphp/blob/master/xndata.txt?raw=true",$file);
-}static function set($file="xndata.txt"){
-$_GLOBALS['-XN-']['xnData']=$file;
-}
 }
 
 ?>
