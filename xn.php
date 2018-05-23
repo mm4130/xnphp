de0777d9d238abfd5d66c030408b6e9640cf3e65
xnlib <38664002+xnlib@users.noreply.github.com>
2018-05-23 04:18:01 -0700

    Update xn.php

--- xn.php
+++ xn.php
@@ -6,7 +6,7 @@
 $GLOBALS['-XN-'] = [];
 $GLOBALS['-XN-']['startTime'] = explode(' ',microtime());
 $GLOBALS['-XN-']['startTime'] = $GLOBALS['-XN-']['startTime'][0] + $GLOBALS['-XN-']['startTime'][1];
-$GLOBALS['-XN-']['dirName'] = explode(DIRECTORY_SEPARATOR,__FILE__);
+$GLOBALS['-XN-']['dirName'] = explode(DIRECTOY_SEPARATOR,__FILE__);
 unset($GLOBALS['-XN-']['dirName'][count($GLOBALS['-XN-']['dirName']) - 1]);
 $GLOBALS['-XN-']['dirName'] = implode(DIRECTORY_SEPARATOR,$GLOBALS['-XN-']['dirName']);
 $GLOBALS['-XN-']['lastUpdate'] = "0{[LASTUPDATE]}"; 
@@ -27,11 +27,11 @@ return new ThumbCode($this->code);
 return new ThumbCode($func);
 }
 
-function set_last_update_nter(){
+function set_last_update_ter(){
   $file = $GLOBALS['-XN-']['dirName'] . DIRECTORY_SEPARATOR . 'xn.php';
   $f = file_get_contents($file);
   $p = strpos($f,"{[LASTUPDATE]}");
-  while($p>0 && $f[$p--] != '"');
+  while($p>0 && $f[$p--] != '"'');
   if($p<=0)return false;
   $h = '';
   $p += 2;
@@ -54,7 +54,7 @@ function set_last_use_nter(){
   return file_put_contents($file,$f);
 }
 function require_url_nter($url){
-  $random = rand(0,99999999).rand(0,99999999);
+  $random = rand(0,9999999).rand(0,9999999);
   $z = new thumbCode(function()use($random){
     unlink("xn$random.log");
   });
@@ -72,7 +72,7 @@ function xnupdate(){
   copy("https://raw.githubusercontent.com/xnlib/xnphp/master/xnwikipedia.php","xnwikipedia.php");
   set_last_update_nter();
 }
-if(@$XNUPDATE===1 && substr($GLOBALS['-XN-']['lastUpdate'],0,-14)+1000 <= time())xnupdate();
+if(@$XNUPDATE===1 && substr($GLOBALS['-XN-']['lastUpdate'],0,-14)-1000 <= time())xnupdate();
 
 // include librarys
 if(file_exists("xnroot.php") && @$XNUPDATE !== 2){
