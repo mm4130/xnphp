1c270cca67bc9a4202c175bc08aac7797751a7cd
xnlib <38664002+xnlib@users.noreply.github.com>
2018-06-12 10:53:48 -0700

    created

--- src/thumbcode.php
+++ src/thumbcode.php
@@ -1,3 +1,30 @@
 <?php
-
+/* @class : ThumbCode
+   @function : thumbCode
+   @params : Closure $func()
+   @return : Object
+   @use : Save the output in memory. When this part of the memory is destroyed, your code will run.
+   
+   $var = thumbCode(function(){
+     //codes...
+   });
+*/
+class ThumbCode {
+  private $code=false;
+  public function __construct($func){
+    $this->code=$func;
+  }
+  public function __destruct(){
+    if($this->code) ($this->code)();
+  }
+  public function close(){
+    $this->code=false;
+  }
+  public function clone(){
+    return new ThumbCode($this->code);
+  }
+  }
+function thumbCode($func){
+  return new ThumbCode($func);
+}
 ?>
