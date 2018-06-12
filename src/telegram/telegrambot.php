40ed75f6e93fa3b79de2482a436670c15503fa71
xnlib <38664002+xnlib@users.noreply.github.com>
2018-06-12 11:28:30 -0700

    Create

---
+++ src/telegram/telegrambot.php
@@ -0,0 +1,13 @@
+<?php
+/* TelegramBot Class
+ * params : String token = ''
+ * use : you can make a API robot on telegram by BotFather API_KEY (token)
+ 
+ $bot = new xn\Telegram\TelegramBot( string );
+ */
+
+namespace xn\Telegram;
+
+
+
+?>
