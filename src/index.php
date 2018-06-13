22b94978826b9be38ba3739950019b1827e515f2
xnlib <38664002+xnlib@users.noreply.github.com>
2018-06-13 03:23:16 -0700

    Create

--- src/index.php
+++ src/index.php
@@ -20,17 +20,25 @@ $GLOBALS['-XN-']['lastUse'] = "0{[LASTUSE]}";
 $GLOBALS['-XN-']['DATA'] = "W10={[DATA]}"; // Assumption = []
 $DATA = json_decode(base64_decode(substr($GLOBALS['-XN-']['DATA'], 0, -8)), @$XNDATA === 1);
 
+// \xn
 require $GLOBALS['-XN-']['dirNameDir'] . "thumbcode.php";
 require $GLOBALS['-XN-']['dirNameDir'] . "nter.php";
+require $GLOBALS['-XN-']['dirNameDir'] . "error.php";
 
+// \xn\Telegram
 require $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "index.php";
+// \xn\Coding
 require $GLOBALS['-XN-']['dirNameDir'] . "coding" . DIRECTORY_SEPARATOR . "index.php";
+// \xn\File
 require $GLOBALS['-XN-']['dirNameDir'] . "files" . DIRECTORY_SEPARATOR . "index.php";
+// \xn\Calc
 require $GLOBALS['-XN-']['dirNameDir'] . "calc" . DIRECTORY_SEPARATOR . "index.php";
+// \xn\API
 require $GLOBALS['-XN-']['dirNameDir'] . "api" . DIRECTORY_SEPARATOR . "index.php";
+// \xn\CF
 require $GLOBALS['-XN-']['dirNameDir'] . "cfile" . DIRECTORY_SEPARATOR . "index.php";
+// \xn\Time
 require $GLOBALS['-XN-']['dirNameDir'] . "time.php";
-require $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "index.php";
 
 $GLOBALS['-XN-']['endTime'] = microtime(1);
 
