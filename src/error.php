c381f36b357a6b47c79d80f3234750d775d3ee6c
xnlib <38664002+xnlib@users.noreply.github.com>
2018-06-16 10:09:02 -0700

    Create

--- src/error.php
+++ src/error.php
@@ -1,20 +1,73 @@
 <?php
 
+/* XNError for show errors
+ * params : String from, (from method/function/....)
+            String message,
+            Integer error_level = 0,
+            Boolean end_code = false
+ * errors level :
+   0 : Warning
+   1 : Notic
+   2 : Lag
+   3 : Status
+   4 : User Error
+   5 : User Warning
+   6 : User Notic
+   7 : Recoverable Error
+   8 : Syntax Error
+   9 : Unexpected
+   10 : Undefined
+   11 : Anonimouse
+   12 : System Error
+   13 : Secury Error
+   14 : Fatal Error
+   15 : Arithmetic Error
+   16 : Parse Error
+   17 : Type Error
+ * use : 
+   
+   new XNError( String , String , Integer , Boolean );
+ ! you can use throwing :
+   
+   throw new XNError( String , String , Integer , Boolean );
+  ! xn always use this Class for display errors
+ */
 class XNError extends Error {
+  // public $HTMLMessage
+  // public $consoleMessage
   protected $message;
   
+  /* setting show errors (static method)
+   * params : Boolean|NULL|String show
+                           true : show xnerrors in output
+                           false : not show xnerrors in output
+                           null : (Assumption) toggle show xnerrors in output
+                           String file : show errors and save in file
+   * use :
+   
+   XNError::show( Boolean|NULL|String );
+   */
   static function show($sh = null) {
     if($sh===null)$GLOBALS['-XN-']['errorShow'] = ! $GLOBALS['-XN-']['errorShow'];
     else $GLOBALS['-XN-']['errorShow'] = $sh;
   }
-  static function handlr($func) {
-    $GLOBALS['-XN-']['errorHandlr'] = $func;
+  /* errors handler (static method)
+   * params : Object(Closure) handle
+   * closure input : Object(XNError) error
+   * use :
+   
+   XNError::handler( function( Object(XNError) ){
+     // codes
+   }
+   */
+  static function handler($func) {
+    $GLOBALS['-XN-']['errorHandler'] = $func;
   }
   public function __construct($in, $text, $level = 0, $en = false) {
     $type    = [
                   "Warning",
                   "Notic",
-                  "Log",
+                  "Lag",
                   "Status",
                   "User Error",
                   "User Warning",
@@ -41,9 +94,10 @@ class XNError extends Error {
     $this->HTMLMessage    = $message;
     $this->consoleMessage = $console;
     $this->message        = "XN $type > $in : $text";
-    if(isset($GLOBALS['-XN-']['errorHandlr'])) {
+    if(isset($GLOBALS['-XN-']['errorHandler'])) {
       try{
-        $GLOBALS['-XN-']['errorHandlr'] ($this);
+        if(is_function($GLOBALS['-XN-']['errorHandler']))
+          $GLOBALS['-XN-']['errorHandler'] ($this);
       }catch(Error $e) { }
       catch(Expection $e) { }
       catch(XNError $e) { }
@@ -52,6 +106,7 @@ class XNError extends Error {
     if($GLOBALS['-XN-']['errorShow']&&is_string($GLOBALS['-XN-']['errorShow']))fadd($GLOBALS['-XN-']['errorShow'],$console);
     if($en)exit;
   }
+  // give Error message
   public function __toString() {
     return $this->message;
   }
