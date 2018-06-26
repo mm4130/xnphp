865d174523e4376b4bf3975aeec0c7806406163f
xnlib <38664002+xnlib@users.noreply.github.com>
2018-06-26 12:07:44 -0700

    Created

---
+++ src/telegram/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// \xn\Telegram\TelegramBot
+require_once $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "telegrambot.php";
+// \xn\Telegram\TelegramLink
+require_once $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "telegramlink.php";
+// \xn\Telegram\TelegramCode
+require_once $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "telegramcode.php";
+// \xn\Telegram\TelegramUploader
+require_once $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "telegramuploader.php";
+// \xn\Telegram\PWRTelegram
+require_once $GLOBALS['-XN-']['dirNameDir'] . "telegram" . DIRECTORY_SEPARATOR . "pwrtelegram.php";
+
+?>
