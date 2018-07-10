93b6a1fbef7c754d94e7f8e8e9f735443e6ed2fd
xnlib <38664002+xnlib@users.noreply.github.com>
2018-07-10 14:56:49 -0700

    Created

--- src/telegram/settings/telegramBot/saveMsgs.php
+++ src/telegram/settings/telegramBot/saveMsgs.php
@@ -2,23 +2,32 @@
 
 namespace xn\Telegram\Setting\TelegramBot\saveMsgs;
 
+use xn;
+
+// save messages for easy use
+// $bot->msgs
 class TelegramBotSaveMsgs {
-private $msgs=[];
-public function get(string $name){
-return isset($this->msgs[$name])?$this->msgs[$name]:false;
-}public function add(string $name,$message){
-$message = XNString::toString($message);
-$this->msgs[$name]=$message;
-return $this;
-}public function delete(string $name){
-if(isset($this->msgs[$name]))
-unset($this->msgs[$name]);
-return $this;
-}public function reset(){
-$this->msgs=[];
-return $this;
-}public function exists(string $name){
-return isset($this->msgs[$name]);
+  private $msgs = [];
+  public function get(string $name) {
+    return isset($this->msgs[$name])? $this->msgs[$name]: false;
+  }
+  public function add(string $name, $message) {
+    $message = xn\string::toString($message);
+    $this->msgs[$name] = $message;
+    return $this;
+  }
+  public function delete(string $name) {
+    if(isset($this->msgs[$name]))
+    unset($this->msgs[$name]);
+    return $this;
+  }
+  public function reset() {
+    $this->msgs = [];
+    return $this;
+  }
+  public function exists(string $name) {
+    return isset($this->msgs[$name]);
+  }
 }
 
 ?>
