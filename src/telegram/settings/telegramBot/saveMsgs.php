d27e67850ce813333dade3e6b721052b032b09d0
xnlib <38664002+xnlib@users.noreply.github.com>
2018-07-10 15:01:24 -0700

    Created

--- src/telegram/settings/telegramBot/saveMsgs.php
+++ src/telegram/settings/telegramBot/saveMsgs.php
@@ -8,23 +8,54 @@ use xn;
 // $bot->msgs
 class TelegramBotSaveMsgs {
   private $msgs = [];
+
+ /* get message
+  * params : String name
+  * use :
+  
+  $bot->msgs->get( String );
+  */
   public function get(string $name) {
     return isset($this->msgs[$name])? $this->msgs[$name]: false;
   }
+  /* add message
+   * params : String name,
+              Mixed message
+   * use :
+   
+   $bot->msgs->add( String , Mixed );
+   */
   public function add(string $name, $message) {
     $message = xn\string::toString($message);
     $this->msgs[$name] = $message;
     return $this;
   }
+  /* delete message
+   * params : String name
+   * use :
+   
+   $bot->msgs->delete( String );
+   */
   public function delete(string $name) {
     if(isset($this->msgs[$name]))
     unset($this->msgs[$name]);
     return $this;
   }
+  /* delete all messages
+   * use :
+   
+   $bot->msgs->reset();
+   */
   public function reset() {
     $this->msgs = [];
     return $this;
   }
+  /* exists message name
+   * params : String name
+   * use :
+   
+   $bot->msgs->exists( String );
+   */
   public function exists(string $name) {
     return isset($this->msgs[$name]);
   }
