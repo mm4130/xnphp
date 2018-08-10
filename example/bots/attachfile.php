69d1c271612c2b1f75284034d38e0fead40bb06d
xnlib <38664002+xnlib@users.noreply.github.com>
2018-08-10 14:01:20 -0700

    Created

--- example/bots/attachfile.php
+++ example/bots/attachfile.php
@@ -3,6 +3,7 @@
 require "xn.php";
 
 $bot = new TelegramBot("TOKEN");
+
 $update = $bot->update;
 $bot->unfilterUpdates(["message"]);
 $update = $update->message;
@@ -17,16 +18,16 @@ $chat = $update->chat;
 $data = xndata::file("attachfile.txt");
 
 if($text == "/start")
-  $bot->sendMessage($chat->id,"Hello %message.first_name%!\nWelcom to Attach robot.\nYou can send File and go to Attaching Message");
+    $bot->sendMessage($chat->id,"Hello %message.first_name%!\nWelcom to Attach robot.\nYou can send File and go to Attaching Message");
 elseif($data->iskey($chat->id) && $text){
-  $bot->sendMessage($chat->id,"[\x0c](http://t.me/tebrobot/".$data->value($chat->id).$text,["mode"=>"MarkDown"]);
-  $data->delete($chat->id);
+    $bot->sendMessage($chat->id,"[\x0c](http://t.me/tebrobot/".$data->value($chat->id).$text,["mode"=>"MarkDown"]);
+    $data->delete($chat->id);
 }
 else{
-  $bot->sendMessage($chat->id,"OK!\nnow send to me a text message for attaching on that");
-  if($text)
-    $data->set($chat->id,TelegramUploader::attach($text,"text");
-  $data->set($chat->id,TelegramUploader::attach($bot->getFileInfo()->file_id
+    $bot->sendMessage($chat->id,"OK!\nnow send to me a text message for attaching on that");
+    if($text)
+        $data->set($chat->id,TelegramUploader::attach($text,"text"));
+    $data->set($chat->id,TelegramUploader::attach($bot->getFileInfo()->file_id));
 }
 
 ?>
